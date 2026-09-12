56d0b74bcd9b040cefa3fde592231c690b9b0fef
Schalk Dormehl <schalk.dormehl@gmail.com>
2026-09-12 14:30:40 +0200

    Update init test for Schelm command name

--- test/Test/Lamdera.hs
+++ test/Test/Lamdera.hs
@@ -45,7 +45,7 @@ suite = tests
             "Hello! Lamdera projects always start with an elm.json file, as well as four\\nsource files: Frontend.elm , Backend.elm , Types.elm and Env.elm\\n\\nIf you're new to Elm, the best starting point is\\n<https://elm-lang.org/0.19.1/init>\\n\\nOtherwise check out <https://dashboard.lamdera.app/docs/building> for Lamdera\\nspecific information!\\n\\nKnowing all that, would you like me to create a starter implementation? [Y/n]:"
 
           expectTextContains actual
-            "Okay, I created it! Now read those links, or get going with `lamdera live`.\\n"
+            "Okay, I created it! Now read those links, or get going with `schelm live`.\\n"
 
       in
       using setup cleanup test
